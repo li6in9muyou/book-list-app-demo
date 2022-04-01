30efcd9863768a93766e0be97fb8d54fb6566c4a
li6in9muyou <libq5520@mails.jlu.edu.cn>
2022-04-01 20:10:42 +0800

    fix: only render modal if shouldShow === true

--- src/components/AddToBookListModal.svelte
+++ src/components/AddToBookListModal.svelte
@@ -1,20 +1,27 @@
 <script>
     import CreateBookList from './CreateBookList.svelte'
     import ListUserBookLists from './ListUserBookLists.svelte'
+    import MdClose from 'svelte-icons/md/MdClose.svelte'
     import {setContext} from 'svelte'
 
     export let shouldShow = false
     setContext('pleaseCloseModal', () => shouldShow = false)
 </script>
 
-<div class="modal" class:modal-open={shouldShow}>
-    <div class="modal-box relative max-w-sm">
-        <label class="btn btn-sm btn-circle absolute right-2 top-2"
-               on:click="{() => shouldShow = false}">✕</label>
-        <h3 class="text-lg font-bold">添加到书单……</h3>
-        <div class="divider"></div>
-        <ListUserBookLists/>
-        <div class="divider"></div>
-        <CreateBookList/>
+{#if shouldShow}
+    <div class="modal modal-open">
+        <div class="modal-box relative max-w-sm">
+            <label class="btn btn-xs btn-circle btn-accent absolute right-2 top-2"
+                   on:click="{() => shouldShow = false}">
+                <span class="w-6 h-6">
+                    <MdClose/>
+                </span>
+            </label>
+            <h3 class="text-lg font-bold">添加到书单……</h3>
+            <div class="divider"></div>
+            <ListUserBookLists/>
+            <div class="divider"></div>
+            <CreateBookList/>
+        </div>
     </div>
-</div>
+{/if}
