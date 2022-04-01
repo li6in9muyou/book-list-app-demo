f8e037126b07a5eda0a69ba50400d40eb0f590e3
li6in9muyou <libq5520@mails.jlu.edu.cn>
2022-04-01 09:39:13 +0800

    feat: add app-wide notification

--- src/main.js
+++ src/main.js
@@ -1,15 +1,15 @@
 import './assets/app.css'
-import ActionPanel from "./components/ActionPanel.svelte";
+import App from './App.svelte'
 
-const app = new ActionPanel({
+const app = new App({
     target: document.body,
     props: {
         book: {
             id: 999,
-            path: "some book.pdf"
+            path: 'some book.pdf'
         },
         subject: 999
     }
-});
+})
 
 export default app
