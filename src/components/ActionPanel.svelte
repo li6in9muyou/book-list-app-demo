0189decc455be363a688c279544567fc6b779a4d
li6in9muyou <libq5520@mails.jlu.edu.cn>
2022-04-01 19:45:25 +0800

    fix: icon and text now sit on baseline

--- src/components/ActionPanel.svelte
+++ src/components/ActionPanel.svelte
@@ -1,71 +1,49 @@
-<style lang="postcss">
-    .actionButton {
-        @apply btn;
-        @apply min-w-fit;
-    }
-</style>
 <script>
-    import TextDisplay from "../lib/TextDisplay.svelte";
+    import {setContext} from 'svelte'
+    import FaArchive from 'svelte-icons/fa/FaArchive.svelte'
+    import FaInfoCircle from 'svelte-icons/fa/FaInfoCircle.svelte'
+    import FaArrowUp from 'svelte-icons/fa/FaArrowUp.svelte'
+    import AddToBookListModal from './AddToBookListModal.svelte'
+    import BookDetail from './BookDetail.svelte'
 
-    export let subject;
-    import {repeat} from 'lodash'
+    export let subject = -1
+    setContext('thisBook', subject)
 
-    let cachedDetail = ''
+    let shouldShowAddToBookListModal = false
     let shouldShowDetail = false
-
-    async function fetch_detail(sub) {
-        if (cachedDetail !== '') {
-            return cachedDetail
-        }
-        const dd = `关于这本书（id: ${sub}） 的简要介绍，但是有点${repeat('长', 999)}。`
-        await new Promise((resolve => setTimeout(resolve, 500)))
-        cachedDetail = dd
-        return dd
-    }
-
-    async function fetch_bookList(sub) {
-        return await (await fetch(`http://localhost:8081/api/${sub}/book-lists`)).json()
-    }
-
-    let current_user = 'li6q'
-    let shouldShowBookList = false;
-
 </script>
 <div>
-    <main class="">
-        <button class="actionButton"
-                on:click={()=>{}}>
-            加入箩筐
-        </button>
-        <button class="actionButton"
-                on:click={() => shouldShowBookList = !shouldShowBookList}>
-            加入书单
-        </button>
-        <button class="actionButton"
-                on:click={() => shouldShowDetail = !shouldShowDetail}>
+    <main class="space-x-6">
+        <label class="btn btn-secondary btn-outline btn-sm content-center items-end">
+            <input bind:checked={shouldShowAddToBookListModal} class="hidden" type="checkbox">
+            <span class="h-4 w-4">
+                <FaArchive/>
+            </span>
+            <span class="ml-2">
+                加入书单
+            </span>
+        </label>
+        <label class="btn btn-secondary btn-outline btn-sm content-center items-end">
+            <input bind:checked={shouldShowDetail} class="hidden" type="checkbox">
             {#if shouldShowDetail}
-                收起
+                <span class="h-4 w-4">
+                    <FaArrowUp/>
+                </span>
+                <span class="ml-2">
+                    收起
+                </span>
             {:else}
-                查看详情
+                <span class="h-4 w-4">
+                    <FaInfoCircle/>
+                </span>
+                <span class="ml-2">
+                    查看详情
+                </span>
             {/if}
-        </button>
+        </label>
     </main>
     <comment>
-        {#if shouldShowBookList}
-            {#await fetch_bookList(current_user)}
-                <h2>书单加载中……</h2>
-            {:then bookLists}
-                {#each bookLists as list}
-                    <h2>{list.title}</h2>
-                {/each}
-            {/await}
-        {/if}
-        {#if shouldShowDetail}
-            {#await fetch_detail(subject)}
-                <h2>详情加载中……</h2>
-            {:then detail}
-                <TextDisplay text="{detail}"/>
-            {/await}
-        {/if}
+        <AddToBookListModal bind:shouldShow="{shouldShowAddToBookListModal}"/>
+        <BookDetail bind:shouldShow="{shouldShowDetail}" thisBook={subject}/>
     </comment>
 </div>
