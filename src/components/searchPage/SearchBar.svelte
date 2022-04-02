793534cd67be950114a6772bd19cf705f892ed5a
li6in9muyou <libq5520@mails.jlu.edu.cn>
2022-04-02 16:16:51 +0800

    feat: extracted search bar

---
+++ src/components/searchPage/SearchBar.svelte
@@ -0,0 +1,53 @@
+<script>
+    import {textSearch} from '../searchEengine.js'
+
+    export let hay = []
+    export let results
+    export let error
+    let query = ''
+
+    let success
+    let showing_count
+    let ignoreCase = true
+    let useRegex = true
+
+
+    $: {
+        ({results, success, error} = textSearch(
+                query, hay, e => e,
+                {
+                    useRegex: useRegex,
+                    ignoreCase: ignoreCase
+                }
+            )
+        )
+    }
+    $: showing_count = results.length
+</script>
+
+<div class="grid gap-4 items-center items-stretch
+            sm:flex">
+    <input autocapitalize="off"
+           autocomplete="off"
+           bind:value={query}
+           class="font-mono input input-secondary input-bordered shadow
+                  md:text-xl
+                  sm:flex-1
+                  md:flex-1 md:w-1/3
+                  max-w-lg"
+           placeholder="搜索……"
+           spellcheck="false"
+           type="text">
+    <label class="label cursor-pointer border rounded shadow
+                  sm:gap-2">
+        <span class="label-text">用正则表达</span>
+        <input bind:checked={useRegex} class="toggle toggle-primary"
+               type="checkbox">
+    </label>
+    <label class="label cursor-pointer border rounded shadow
+                  sm:gap-2">
+        <span class="label-text">忽略大小写</span>
+        <input bind:checked={ignoreCase} class="toggle toggle-primary"
+               type="checkbox">
+    </label>
+</div>
