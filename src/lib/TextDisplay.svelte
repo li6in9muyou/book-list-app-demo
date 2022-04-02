acbc14e8bf0946d8e90e2fb0bff9929e239b3f4c
li6in9muyou <libq5520@mails.jlu.edu.cn>
2022-04-02 17:39:06 +0800

    fix: remove margins

--- src/lib/TextDisplay.svelte
+++ src/lib/TextDisplay.svelte
@@ -3,7 +3,7 @@
 </script>
 
 <article
-  class="mt-4 p-4 bg-base-200 text-base-content
+  class="p-4 bg-base-200 text-base-content
                 max-h-60 overflow-y-auto rounded-lg font-sans
                 shadow-lg"
 >
