7abd202e5455862bfadab0c063b791370244a64a
li6in9muyou <libq5520@mails.jlu.edu.cn>
2022-04-03 11:06:39 +0800

    feat: add endpoint for checking email existence

--- src/lib/UserService.js
+++ src/lib/UserService.js
@@ -1,3 +1,10 @@
 import { readable } from "svelte/store";
+import { isEmpty } from "lodash/lang.js";
 
 export const CurrentUser = readable("li6q");
+
+export async function checkEmailExists(email) {
+  const query = import.meta.env.VITE_DEV_DB_URL + `/api/users?email=${email}`;
+  const querySet = await (await fetch(query)).json();
+  return isEmpty(querySet);
+}
