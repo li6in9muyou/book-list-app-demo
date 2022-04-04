d6faf566d5698bcfebf46b5f2d1a9454306cb8b6
li6in9muyou <libq5520@mails.jlu.edu.cn>
2022-04-04 08:34:05 +0800

    feat: add endpoint for signing up

--- src/lib/UserService.js
+++ src/lib/UserService.js
@@ -1,10 +1,24 @@
 import { readable } from "svelte/store";
 import { isEmpty } from "lodash/lang.js";
+import { debounce } from "lodash/function.js";
 
 export const CurrentUser = readable("li6q");
 
-export async function checkEmailExists(email) {
+export const checkEmailExists = debounce(async (email) => {
   const query = import.meta.env.VITE_DEV_DB_URL + `/api/users?email=${email}`;
   const querySet = await (await fetch(query)).json();
   return isEmpty(querySet);
+}, 500);
+
+export async function createUser(eml, pwd) {
+  const q = await fetch(import.meta.env.VITE_DEV_DB_URL + `/api/users/`, {
+    method: "POST",
+    body: JSON.stringify({
+      email: eml,
+      password: pwd,
+    }),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
 }
