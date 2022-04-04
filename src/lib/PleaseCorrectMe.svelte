fe9feb2c4b71f75fb23849e4b35b5087292aa650
li6in9muyou <libq5520@mails.jlu.edu.cn>
2022-04-04 08:32:40 +0800

    utility: form input error tip

---
+++ src/lib/PleaseCorrectMe.svelte
@@ -0,0 +1,15 @@
+<script>
+  export let predicate = () => true;
+  export let prompt = "这一项填写错了";
+  import { fade } from "svelte/transition";
+</script>
+
+{#if predicate()}
+  <span class="label-text-alt px-2" transition:fade>
+    <span
+      class="text-error underline underline-offset-2 font-bold decoration-2"
+    >
+      {prompt}
+    </span>
+  </span>
+{/if}
