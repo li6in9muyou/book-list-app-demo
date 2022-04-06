7fee84236a10a3f9f4b98251459d2693f4238be9
li6in9muyou <libq5520@mails.jlu.edu.cn>
2022-04-06 20:16:56 +0800

    service: add persist services

--- src/lib/backendService/UserService.js
+++ src/lib/backendService/UserService.js
@@ -1,14 +1,26 @@
-import { readable } from "svelte/store";
+import { derived, writable } from "svelte/store";
 import { isEmpty } from "lodash/lang.js";
 import { debounce } from "lodash/function.js";
-
-export const CurrentUser = readable("li6q");
+import { subscribe } from "svelte/internal";
+import { split } from "lodash";
 
 export const checkEmailExists = debounce(async (email) => {
   const query = import.meta.env.VITE_DEV_DB_URL + `/api/users?email=${email}`;
   const querySet = await (await fetch(query)).json();
   return isEmpty(querySet);
 }, 500);
+
+export function successToken(obj) {
+  return obj.user !== undefined && obj.accessToken !== undefined;
+}
+
+export function persistUser(obj) {
+  const { user: userInfo, accessToken } = obj;
+  if (userInfo === undefined || accessToken === undefined) {
+  }
+  CurrentAccessToken.set(accessToken);
+  CurrentUser.set(userInfo);
+}
 
 export async function createUser(eml, pwd) {
   const q = await fetch(import.meta.env.VITE_DEV_DB_URL + `/api/users/`, {
@@ -21,4 +33,47 @@ export async function createUser(eml, pwd) {
       "Content-Type": "application/json",
     },
   });
+  return await q.json();
 }
+
+export async function loginUser(eml, pwd) {
+  const q = await fetch(import.meta.env.VITE_DEV_DB_URL + "/login", {
+    method: "POST",
+    body: JSON.stringify({
+      email: eml,
+      password: pwd,
+    }),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+  return await q.json();
+}
+
+export const CurrentUser = writable({}, (set) => {
+  set(JSON.parse(localStorage.getItem("userInfo")));
+});
+
+export const CurrentAccessToken = writable("", (set) => {
+  set(JSON.parse(localStorage.getItem("accessToken")));
+});
+
+export const redirectUrl = writable("/");
+
+subscribe(CurrentUser, (value) => {
+  localStorage.setItem("userInfo", JSON.stringify(value));
+});
+
+subscribe(CurrentAccessToken, (value) => {
+  localStorage.setItem("accessToken", JSON.stringify(value));
+});
+
+export const CurrentUserDisplayName = derived(CurrentUser, (cu) => {
+  if (cu == null) {
+    return "";
+  } else {
+    return split(cu.email, "@")[0];
+  }
+});
+
+export const isAuthenticated = derived(CurrentUser, (cu) => cu !== null);
