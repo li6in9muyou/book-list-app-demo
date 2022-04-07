94d3ec03768d2d79a36a3fb1e0c939ef1f3eb55d
li6in9muyou <libq5520@mails.jlu.edu.cn>
2022-04-07 13:00:54 +0800

    minor: remove unused props

--- src/main.js
+++ src/main.js
@@ -3,13 +3,6 @@ import App from "./App.svelte";
 
 const app = new App({
   target: document.body,
-  props: {
-    book: {
-      id: 999,
-      path: "some book.pdf",
-    },
-    subject: 999,
-  },
 });
 
 export default app;
