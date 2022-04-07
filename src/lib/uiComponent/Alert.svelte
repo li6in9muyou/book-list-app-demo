aa7585c2b3254d52b883cd50ac0648b96ab32990
li6in9muyou <libq5520@mails.jlu.edu.cn>
2022-04-07 12:54:31 +0800

    fix: background is transparent

--- src/lib/uiComponent/Alert.svelte
+++ src/lib/uiComponent/Alert.svelte
@@ -10,7 +10,7 @@
 <div
   in:fly={{ x: -400, duration: 1000 }}
   out:scale
-  class="z-40 m-4 ml-auto flex w-fit max-w-xs flex-row items-center
+  class="alert z-40 m-4 ml-auto flex w-fit max-w-xs flex-row items-center
    justify-between rounded-lg rounded p-2 px-4 shadow shadow-lg
    md:relative md:right-1/3 xl:relative xl:right-96"
   class:alert-error={"error" === color}
