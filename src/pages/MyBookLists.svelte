0b332ac7c55a30b24f447d6735f91ffec6141628
li6in9muyou <libq5520@mails.jlu.edu.cn>
2022-04-09 23:41:38 +0800

    feat: link myBookLists page into routes

--- src/pages/MyBookLists.svelte
+++ src/pages/MyBookLists.svelte
@@ -2,6 +2,10 @@
   import { CurrentUser } from "../lib/backendService/UserService.js";
 </script>
 
-<div class="bg-primary text-xl text-primary-content">
-  我的书单页面，我是{$CurrentUser}
+<div class="p-4">
+  <div
+    class="break-all rounded rounded-lg bg-primary p-4 text-xl text-primary-content"
+  >
+    我的书单页面，我是{$CurrentUser}
+  </div>
 </div>
