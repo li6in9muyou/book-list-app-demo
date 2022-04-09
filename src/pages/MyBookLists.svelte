c0dfac4f803bc616c760c3209b6fff432d84bb22
li6in9muyou <libq5520@mails.jlu.edu.cn>
2022-04-09 22:16:41 +0800

    feat: create a page listing personal book-lists

---
+++ src/pages/MyBookLists.svelte
@@ -0,0 +1,7 @@
+<script>
+  import { CurrentUser } from "../lib/backendService/UserService.js";
+</script>
+
+<div class="bg-primary text-xl text-primary-content">
+  我的书单页面，我是{$CurrentUser}
+</div>
