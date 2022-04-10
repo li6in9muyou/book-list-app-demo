f2635149e5432e4ec49d241eeabb8930012b13a3
li6in9muyou <libq5520@mails.jlu.edu.cn>
2022-04-10 09:41:19 +0800

    fix: use native fonts

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,12 +1,12 @@
 module.exports = {
-    content: ['./src/**/*.svelte'],
-    theme: {
-        extend: {},
-        fontFamily: {
-            'sans': ['poppins', 'noto_sans_sc'],
-            'serif': ['biz_udpmincho', 'noto_serif_sc'],
-            'mono': ['ubuntu_mono', 'noto_sans_sc']
-        }
+  content: ["./src/**/*.svelte"],
+  theme: {
+    extend: {},
+    fontFamily: {
+      sans: ["poppins", "BlinkMacSystemFont"],
+      serif: ["STZhongsong", "STSong"],
+      mono: ["ubuntu_mono", "STZhongsong"],
     },
-    plugins: [require('daisyui')]
-}
+  },
+  plugins: [require("daisyui")],
+};
