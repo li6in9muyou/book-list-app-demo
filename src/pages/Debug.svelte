15cd0e7d4528e83810f3a30c0be8f086dbe9b3e9
li6in9muyou <libq5520@mails.jlu.edu.cn>
2022-04-10 16:41:57 +0800

    feat: prototype of my booklist page

--- src/pages/Debug.svelte
+++ src/pages/Debug.svelte
@@ -2,39 +2,43 @@
   import { fetchAllBookListOfUser } from "../lib/backendService/BookList.service";
   import { get } from "lodash/object.js";
   import AddToBookListModal from "../components/bookListModal/AddToBookListModal.svelte";
+  import { CurrentUser } from "../lib/backendService/user.service";
+  import EmbeddedAlert from "../lib/uiComponent/EmbeddedAlert.svelte";
 
-  let shouldShow = true;
+  let shouldShow = false;
+  let thisBook = 1111;
 </script>
 
-<AddToBookListModal bind:shouldShow />
+<AddToBookListModal bind:shouldShow {thisBook} />
 <div class="p-4">
+  <input
+    type="number"
+    class="input-outline input w-1/2 border font-mono text-xl shadow"
+    bind:value={thisBook}
+  />
   <label class="btn btn-ghost mb-4 text-xl text-secondary">
-    书单列表
+    加入书单
     <input bind:checked={shouldShow} class="hidden" type="checkbox" />
   </label>
-  {#await fetchAllBookListOfUser("li6q") then bookLists}
+  {#await fetchAllBookListOfUser($CurrentUser) then bookLists}
     <div class="flex flex-col gap-4">
-      <table class="table w-full">
+      <table class="table w-full table-fixed">
         <thead>
           <tr>
-            <th>书单名称</th>
+            <th colspan="2">书单名称</th>
             <th>创建者</th>
           </tr>
         </thead>
         <tbody>
           {#each bookLists as bookList}
             <tr>
-              {#if bookList.title === undefined}
-                <td>
-                  <em>未命名</em>
-                </td>
-              {:else}
-                <td>"{bookList.title}"</td>
-              {/if}
-              <td>{bookList.userId}</td>
+              <td colspan="2" class="truncate">
+                "{bookList.title || "未命名的书单-" + bookList.id}"
+              </td>
+              <td colspan="1">{bookList.userId}</td>
             </tr>
             <tr>
-              <td colspan="2" class="border-b border-b-2 border-b-accent">
+              <td colspan="3" class="border-b border-b-2 border-b-accent">
                 <div class="flex items-center">
                   书
                   <div class="divider divider-horizontal" />
@@ -52,5 +56,7 @@
         </tbody>
       </table>
     </div>
+  {:catch e}
+    <EmbeddedAlert text={`出错了：${e}}`} />
   {/await}
 </div>
