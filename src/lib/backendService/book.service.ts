b01479e9d98eb9716ceb040dd2dc3c70abbe9dc7
li6in9muyou <libq5520@mails.jlu.edu.cn>
2022-04-11 23:02:18 +0800

    fix: constructor does not init any fields

--- src/lib/backendService/book.service.ts
+++ src/lib/backendService/book.service.ts
@@ -1,10 +1,15 @@
+import { map } from "lodash";
+
 export class Book {
   id: number;
   path: string;
-  constructor({ id, path }: { id: number; path: string }) {}
+  constructor({ id, path }: { id: number; path: string }) {
+    this.id = id;
+    this.path = path;
+  }
 }
 
 export async function fetchAllBooks(): Promise<Book[]> {
   const url = import.meta.env.VITE_DEV_DB_URL + `/api/books`;
-  return (await (await fetch(url)).json()).forEach((book) => new Book(book));
+  return map(await (await fetch(url)).json(), (book) => new Book(book));
 }
