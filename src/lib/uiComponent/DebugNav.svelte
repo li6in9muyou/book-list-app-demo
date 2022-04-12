64b1d9eba3da0fe7f3cd0e0ce9350a6579ced4f1
li6in9muyou <libq5520@mails.jlu.edu.cn>
2022-04-12 21:23:44 +0800

    refactor: add new entries

--- src/lib/uiComponent/DebugNav.svelte
+++ src/lib/uiComponent/DebugNav.svelte
@@ -2,6 +2,7 @@
   import MenuItem from "./MenuItem.svelte";
   import FaBug from "svelte-icons/fa/FaBug.svelte";
   import FaRunning from "svelte-icons/fa/FaRunning.svelte";
+  import DiDatabase from "svelte-icons/di/DiDatabase.svelte";
   import { links } from "../../routes.js";
   import { createEventDispatcher } from "svelte";
   import { authDebugUser } from "../backendService/user.service.js";
@@ -10,20 +11,39 @@
 </script>
 
 <li>
-  <div class="text-error">
-    <a href={links.debugPage}>
+  <div class="dropdown dropdown-end">
+    <label tabindex="0" class="btn btn-sm text-warning">
       <MenuItem text="排错" Icon={FaBug} />
-    </a>
-  </div>
-</li>
-<li>
-  <div
-    class="text-success"
-    on:click={() => {
-      authDebugUser();
-      dispatch("afterLogIn");
-    }}
-  >
-    <MenuItem text="快速登录" Icon={FaRunning} />
+    </label>
+    <ul
+      tabindex="0"
+      class="dropdown-content menu rounded-box relative top-full bg-base-100 p-2 shadow"
+    >
+      <li>
+        <div
+          class="w-full text-success "
+          on:click={() => {
+            authDebugUser();
+            dispatch("afterLogIn");
+          }}
+        >
+          <MenuItem text="快速登录" Icon={FaRunning} />
+        </div>
+      </li>
+      <li>
+        <div class="w-full text-error">
+          <a href={links.debugPage}>
+            <MenuItem text="排错" Icon={FaBug} />
+          </a>
+        </div>
+      </li>
+      <li>
+        <div class="w-full text-info">
+          <a href={links.database}>
+            <MenuItem text="数据库" Icon={DiDatabase} />
+          </a>
+        </div>
+      </li>
+    </ul>
   </div>
 </li>
