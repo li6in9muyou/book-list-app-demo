49f123ede774fe5a2782eaa23e9e87747cfc284f
li6in9muyou <libq5520@mails.jlu.edu.cn>
2022-04-13 19:07:33 +0800

    debug: create, delete, insert-into, remove-from of a bookList

--- src/pages/DebugBookListsPouchDB.svelte
+++ src/pages/DebugBookListsPouchDB.svelte
@@ -3,8 +3,9 @@
   import { last, random, remove, toInteger } from "lodash";
   import { getNotify } from "../lib/utility.js";
   import { getNotificationsContext } from "svelte-notifications";
+  import AddToActiveBucket from "../lib/uiComponent/AddToActiveBookList.svelte";
 
-  const { error, success } = getNotify(
+  const { info, error, success, notify } = getNotify(
     getNotificationsContext().addNotification
   );
 
@@ -12,39 +13,72 @@
 
   let listName = random(5000, 5500).toString() + "-untitled";
   let createdBy = 1000;
-  const books = [];
+  const listingBooks = [];
   for (let i = 0; i < 15; i++) {
-    books.push(random(1000, 1800, false));
+    listingBooks.push(random(1000, 1800, false));
   }
+  const genId = (user, list) => `${user} ${list}`;
+  const getList = (id) => last(id.split(" "));
+  const handleBookChange = (ev) => {
+    let { bookId: subject, expected: shouldInList } = ev.detail;
+    subject = toInteger(subject);
+    db.get(genId(createdBy, listName))
+      .then((doc) => {
+        if (shouldInList) {
+          doc.books.push(subject);
+        } else {
+          remove(doc.books, (b) => b === subject);
+        }
 
-  let toDelete = last(books);
-  const handleCreateBookList = () => {
-    console.log("created: ", listName);
-    db.put({
-      _id: listName,
-      books,
-      createdBy,
-    }).catch((err) => {
-      console.log(err);
-      error(err.message);
-    });
+        db.put({
+          books: doc.books,
+
+          _id: doc._id,
+          _rev: doc._rev,
+          createdBy,
+        }).then(() => {
+          notify(shouldInList ? "添加成功" : "移出成功");
+        });
+      })
+      .catch((err) => {
+        const { docId, message, name } = err;
+        console.log(err);
+        error([docId, name, message].join(" "));
+      });
   };
-  const handleDeleteBookFromList = async () => {
+  const handleDeleteBookList = async () => {
+    const id = genId(createdBy, listName);
+    db.get(id)
+      .then((doc) => {
+        doc._deleted = true;
+        db.put(doc).then((r) => {
+          notify("成功删除");
+        });
+      })
+      .catch((err) => {
+        console.error("error when deleting", id, err);
+      });
+  };
+
+  const handleCreateBookList = async () => {
+    const id = genId(createdBy, listName);
     try {
-      const doc = await db.get(listName);
-      remove(doc.books, (b) => b === toInteger(toDelete));
-      if ((await db.put(doc)).ok) {
-        success("成功");
-      }
+      await db.get(id);
+      info(`"${getList(id)}"是已经存在的`);
     } catch (e) {
-      error(e.message);
+      db.put({
+        books: [],
+
+        _id: id,
+        createdBy,
+      }).then(() => notify(`成功创建：${listName}`));
     }
   };
 </script>
 
 <div class="flex flex-col gap-4 p-4">
   <div class="input-group">
-    <span>我是</span>
+    <button class="btn btn-disabled btn-square text-base-content">我是</button>
     <input
       type="number"
       placeholder="用户id"
@@ -55,29 +89,39 @@
   <div class="input-group">
     <input
       type="text"
-      placeholder="书id"
+      placeholder="书单名字"
       class="input input-bordered"
-      bind:value={toDelete}
+      bind:value={listName}
     />
-    <button class="btn btn-square" on:click={handleDeleteBookFromList}>
+    <button
+      class="btn btn-error btn-square min-w-fit px-2"
+      on:click={handleDeleteBookList}
+    >
       删除
+    </button>
+    <button
+      class="btn btn-square min-w-fit px-2"
+      on:click={handleCreateBookList}
+    >
+      新增书单
     </button>
   </div>
   <div class="input-group">
+    <button
+      class="btn btn-disabled btn-square min-w-fit px-2 text-base-content"
+    >
+      使用中的书单
+    </button>
     <input
       type="text"
       placeholder="书单名字"
       class="input input-bordered"
       bind:value={listName}
     />
-    <button class="btn btn-square" on:click={handleCreateBookList}>
-      添加
-    </button>
   </div>
-
   <div class="flex flex-wrap gap-4">
-    {#each books as book}
-      <div class="badge badge-primary badge-outline font-mono">{book}</div>
+    {#each listingBooks as book}
+      <AddToActiveBucket bookId={book} on:bookChange={handleBookChange} />
     {/each}
   </div>
 </div>
