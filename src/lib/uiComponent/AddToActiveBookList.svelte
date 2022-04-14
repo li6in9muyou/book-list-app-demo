482f4b6c183af4a7caa89b3a148c869e25dc9999
li6in9muyou <libq5520@mails.jlu.edu.cn>
2022-04-14 21:16:04 +0800

    debug: use PouchDB to track bookLists

---
+++ src/lib/uiComponent/AddToActiveBookList.svelte
@@ -0,0 +1,17 @@
+<script>
+  import { createEventDispatcher } from "svelte";
+  const dispatch = createEventDispatcher();
+
+  export let bookId = -1;
+  let inList = false;
+  $: {
+    dispatch("bookChange", { bookId, expected: inList });
+  }
+</script>
+
+<label
+  class="badge badge-outline badge-secondary select-none rounded rounded-md font-mono shadow transition-colors duration-300"
+  class:badge-outline={!inList}
+>
+  <input type="checkbox" class="checkbox" bind:checked={inList} />
+</label>
