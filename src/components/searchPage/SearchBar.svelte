fb1f3500834c53cc6396d8af69f1c50bafc4fc63
li6in9muyou <libq5520@mails.jlu.edu.cn>
2022-04-14 22:44:29 +0800

    feat: collapse search options

--- src/components/searchPage/SearchBar.svelte
+++ src/components/searchPage/SearchBar.svelte
@@ -1,12 +1,13 @@
 <script>
   import { textSearch } from "../../lib/searchEengine.ts";
+  import FaCog from "svelte-icons/fa/FaCog.svelte";
   import { identity } from "lodash";
 
   export let hay = [];
   export let results;
   export let error;
   export let getKey = identity;
-  let query = "";
+  let query = "aa";
 
   let success;
   let showing_count;
@@ -22,40 +23,47 @@
   $: showing_count = results.length;
 </script>
 
-<div class="grid items-center items-stretch gap-1 sm:flex">
+<div class="flex">
+  <div class="dropdown">
+    <label
+      tabindex="0"
+      class="btn btn-outline btn-sm rounded-r-none bg-base-300 text-base-content"
+    >
+      <span class="h-6 w-6"><FaCog /></span>
+    </label>
+    <ul
+      tabindex="0"
+      class="dropdown-content top-full z-40 my-2 flex w-52 flex-col gap-2 rounded rounded-lg bg-base-100 p-2"
+    >
+      <li>
+        <label class="label cursor-pointer rounded border px-4 shadow sm:gap-2">
+          <span class="label-text">用正则表达</span>
+          <input
+            bind:checked={useRegex}
+            class="toggle toggle-primary"
+            type="checkbox"
+          />
+        </label>
+      </li>
+      <li>
+        <label class="label cursor-pointer rounded border px-4 shadow sm:gap-2">
+          <span class="label-text">忽略大小写</span>
+          <input
+            bind:checked={ignoreCase}
+            class="toggle toggle-primary"
+            type="checkbox"
+          />
+        </label>
+      </li>
+    </ul>
+  </div>
   <input
     autocapitalize="off"
     autocomplete="off"
     bind:value={query}
-    class="input input-bordered input-secondary font-mono text-lg shadow
-                  sm:flex-1
-                  md:text-xl"
+    class="input input-bordered input-secondary input-sm w-full rounded-l-none font-mono text-lg shadow md:text-xl"
     placeholder="搜索……"
     spellcheck="false"
     type="text"
   />
-  <div class="flex gap-1">
-    <label
-      class="label flex-1 cursor-pointer rounded border px-4 shadow
-                  sm:gap-2"
-    >
-      <span class="label-text">用正则表达</span>
-      <input
-        bind:checked={useRegex}
-        class="toggle toggle-primary"
-        type="checkbox"
-      />
-    </label>
-    <label
-      class="label flex-1 cursor-pointer rounded border px-4 shadow
-                  sm:gap-2"
-    >
-      <span class="label-text">忽略大小写</span>
-      <input
-        bind:checked={ignoreCase}
-        class="toggle toggle-primary"
-        type="checkbox"
-      />
-    </label>
-  </div>
 </div>
