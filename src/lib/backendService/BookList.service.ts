36ae7b53e3493471bf39dcbe421177798b142b0f
li6in9muyou <libq5520@mails.jlu.edu.cn>
2022-04-15 14:48:33 +0800

    feat: migrate BookList data to PouchDB

--- src/lib/backendService/BookList.service.ts
+++ src/lib/backendService/BookList.service.ts
@@ -1,100 +1,122 @@
-import { CurrentUserInfo } from "./user.service";
+import { CurrentUserInfo, CurrentUserId } from "./user.service";
 import { get, Writable, writable } from "svelte/store";
+import { pullAll, concat, last, toNumber } from "lodash";
+import PouchDB from "pouchdb";
 
 export class BookList {
   userId: number;
   title: string;
-  id: string;
   books: number[];
+
+  get id() {
+    return genId(this.userId, this.title);
+  }
+
+  constructor(userId: number, title: string, books: number[]) {
+    this.userId = userId;
+    this.title = title;
+    this.books = books;
+  }
+
+  static fromPouchDocument(doc: {
+    _id: string;
+    createdBy: number;
+    books: number[];
+  }) {
+    const title = last(doc._id.split(" "));
+    return new BookList(doc.createdBy, title, doc.books);
+  }
+}
+
+const db = new PouchDB("dbBookLists");
+
+const genId = (user, list) => `${user} ${list}`;
+
+export async function updateBooksBookList(
+  userId: number,
+  title: string,
+  books: number[],
+  shouldInList: boolean
+) {
+  db.get(genId(userId, title)).then((doc) => {
+    if (shouldInList) {
+      doc.books = [...new Set(concat(doc.books, books))];
+    } else {
+      pullAll(doc.books, books);
+    }
+    db.put({
+      ...doc,
+      _rev: doc._rev,
+    });
+  });
+}
+
+export async function createBookList(
+  userId: number,
+  title: string,
+  books: number[] = []
+) {
+  const id = genId(userId, title);
+  try {
+    await db.get(id);
+  } catch (e) {
+    return await db.put({
+      _id: id,
+      books: books,
+      createdBy: userId,
+      title: title,
+    });
+  }
+  throw { status: 409, message: "bookList already exists" };
+}
+
+export async function deleteBookList(userId: number, title: string) {
+  const id = genId(userId, title);
+  const doc = await db.get(id);
+  doc._deleted = true;
+  await db.put(doc);
 }
 
 export async function BookList_create(title: string, books: number[] = []) {
   const userId = get(CurrentUserInfo).id;
-  return await fetch(import.meta.env.VITE_DEV_DB_URL + `/api/book-lists`, {
-    method: "POST",
-    headers: {
-      Accept: "application/json",
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({
-      title: title,
-      books: books,
-      userId: userId,
-    }),
-  });
+  return await createBookList(userId, title, books);
 }
 
 export async function fetchBookListsByUserId(
   userId: number
 ): Promise<BookList[]> {
-  return await (
-    await fetch(
-      import.meta.env.VITE_DEV_DB_URL + `/api/user/${userId}/book-lists`
-    )
-  ).json();
-}
-
-async function getBookListById(id: string): Promise<BookList> {
-  return await (
-    await fetch(import.meta.env.VITE_DEV_DB_URL + `/api/book-lists/${id}`, {
-      method: "GET",
-      headers: {
-        Accept: "application/json",
-        "Content-Type": "application/json",
-      },
-    })
-  ).json();
+  const doc = await db.allDocs({
+    include_docs: true,
+    startkey: userId.toString(),
+    endkey: userId.toString() + "\ufff0",
+  });
+  console.table(doc.rows);
+  const lists = doc.rows.map(({ doc }) => BookList.fromPouchDocument(doc));
+  console.table(lists);
+  return lists;
 }
 
 export async function BookList_removeBooks(
-  id: string,
+  bookListId: string,
   deadBooks: number[]
-): Promise<any> {
-  const existingBooks = await BookList_fetchBooks(id);
-  return await (
-    await fetch(import.meta.env.VITE_DEV_DB_URL + `/api/book-lists/${id}`, {
-      method: "PATCH",
-      headers: {
-        Accept: "application/json",
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        books: existingBooks.filter((id) => deadBooks.indexOf(id) === -1),
-      }),
-    })
-  ).json();
-}
-
-async function BookList_fetchBooks(id: string): Promise<number[]> {
-  return (await getBookListById(id)).books;
-}
-
-export async function BookList_addBooks(
-  id: string,
-  newBooks: number[]
-): Promise<any> {
-  const existingBooks = await BookList_fetchBooks(id);
-  return await fetch(
-    import.meta.env.VITE_DEV_DB_URL + `/api/book-lists/${id}`,
-    {
-      method: "PATCH",
-      headers: {
-        Accept: "application/json",
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        books: [...newBooks, ...existingBooks],
-      }),
-    }
+): Promise<void> {
+  return updateBooksBookList(
+    get(CurrentUserId),
+    last(bookListId.split(" ")),
+    deadBooks,
+    false
   );
 }
 
-export async function deleteBookList(user, title) {
-  return await fetch(
-    import.meta.env.VITE_DEV_DB_URL + `/api/${user}/book-lists/${title}`,
-    {
-      method: "DELETE",
-    }
+export async function BookList_addBooks(
+  bookListId: string,
+  newBooks: number[]
+): Promise<any> {
+  return updateBooksBookList(
+    get(CurrentUserId),
+    last(bookListId.split(" ")),
+    newBooks,
+    true
   );
 }
 
