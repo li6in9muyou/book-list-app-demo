b2d272ace2d6b0880362984350e19844e5f1860a
li6in9muyou <libq5520@mails.jlu.edu.cn>
2022-04-22 19:29:48 +0800

    feat: speed up

--- src/components/Library/BookBagListing.svelte
+++ src/components/Library/BookBagListing.svelte
@@ -1,42 +1,40 @@
 <script lang="ts">
   import { scale } from "svelte/transition";
   import FaTrashAlt from "svelte-icons/fa/FaTrashAlt.svelte";
+  import VirtualScroll from "@sveltejs/svelte-virtual-list";
   import EmbeddedAlert from "../../lib/uiComponent/EmbeddedAlert.svelte";
   import { isEmpty } from "lodash";
-  import { getContext } from "svelte";
-  import { createEventDispatcher } from "svelte";
+  import { createEventDispatcher, getContext } from "svelte";
+
   const dispatch = createEventDispatcher();
 
   const books = getContext("Selected");
 </script>
 
 <div
-  class="max-h-72 w-full max-w-xs overflow-auto rounded rounded border bg-base-200 py-2 shadow-lg"
+  class="h-72 w-full max-w-xs overflow-auto rounded rounded-lg border bg-base-200 p-2 shadow-lg"
 >
   {#if isEmpty($books)}
     <div class="w-60 p-4">
       <EmbeddedAlert color="info" text="购物车中还没有书" />
     </div>
   {:else}
-    <div class="px-4 flex flex-col gap-y-4 max-w-full items-stretch">
-      {#each $books as book}
-        <div
-          class="flex border-b border-b-neutral px-2 justify-between items-center"
-          transition:scale
-        >
-          <span class="truncate italic">{book.title}</span>
-          <label tabindex="0" class="btn btn-ghost btn-square btn-sm">
-            <span
-              class="h-4 w-4 text-warning"
-              on:click={() => {
-                dispatch("unselect", { book });
-              }}
-            >
-              <FaTrashAlt />
-            </span>
-          </label>
-        </div>
-      {/each}
-    </div>
+    <VirtualScroll items={$books} let:item>
+      <div
+        class="flex border-b border-b-neutral px-2 justify-between items-center"
+      >
+        <span class="truncate italic">{item.title}</span>
+        <label tabindex="0" class="btn btn-ghost btn-square btn-sm">
+          <span
+            class="h-4 w-4 text-warning"
+            on:click={() => {
+              dispatch("unselect", { item });
+            }}
+          >
+            <FaTrashAlt />
+          </span>
+        </label>
+      </div>
+    </VirtualScroll>
   {/if}
 </div>
--- src/components/Library/main.svelte
+++ src/components/Library/main.svelte
@@ -9,7 +9,6 @@
   import BatchOp from "./BatchOp.svelte";
   import BookBagListing from "./BookBagListing.svelte";
   import AddBooksToList from "../BookListComponet/AddBooksToList/main.svelte";
-  import SearchTip from "../searchPage/SearchTip.svelte";
   // depends:
   import { Book, fetchAllBooks } from "../Book/services";
   import { CurrentUserId } from "../../lib/backendService/user.service.js";
